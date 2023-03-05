9cf0a973dc8882439245758b0029e39a4a6fbe61
Justin Buist <justin.buist@gmail.com>
2023-03-04 19:52:21 -0500

    Cleaning house

--- api/HelloWorld/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-import logging
-import azure.functions as func
-
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    data = { 'message': 'Hello World!' }
-    return func.HttpResponse(json.dumps(data), status_code=200)
--- api/process_cli.py
+++
@@ -1,6 +0,0 @@
-from ar_utils import *
-
-pit = get_pit_data('4003data', '2022mifor', 1711)
-print(pit)
-
-
