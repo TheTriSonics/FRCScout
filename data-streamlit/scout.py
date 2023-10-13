5be26cf4ef50e4c16b6807064c162aebf4a4bef3
Justin Buist <justin.buist@gmail.com>
2023-10-13 19:56:09 -0400

    Upped my comment game

--- data-streamlit/scout.py
+++ data-streamlit/scout.py
@@ -29,8 +29,9 @@ def load_pit_data(secret_key, event_key, team_key):
 
 
 st.set_page_config(layout='wide')
-st.title("Event Scouting Data")
+st.title('Trisonics FRC Scouting')
 
+# List of competitions to offer in select box
 event_key_list = ['2023micmp4', '2023miwmi']
 
 secret_key = st.text_input('Secret key')
@@ -38,6 +39,7 @@ event_key = st.selectbox('Event key', event_key_list)
 
 team = None
 
+# This only runs when the user has entered a key and selected an event
 if secret_key and event_key:
     data_load_state = st.text("Loading data...")
     data = load_event_data(secret_key, event_key)
@@ -51,34 +53,47 @@ if show_raw:
     st.write(data)
 
 
+# As earlier, this only runs when a team has been selected
 if team:
-    # Trim team data down from the full event data to just theirs
+    # Trim team data down from the full event data to just theirs w/ Pandas
     tdf = data.loc[data.scouting_team == team]
     # Load in pit data
     print('Loading', team)
     pdf = load_pit_data(secret_key, event_key, team)
+    # Gets a list of every column name in the dataframe
     allcols = tdf.columns
+    # Now we can create new dataframes where we only see the auto columns
     auton_cols = [c for c in allcols if c.startswith('auto')]
     auton_df = tdf[auton_cols]
+    # And then teleop...
     teleop_cols = [c for c in allcols if c.startswith('tele')]
     teleop_df = tdf[teleop_cols]
+    # And endgame.
     endgame_cols = [c for c in allcols if c.startswith('endgame')]
     endgame_df = tdf[endgame_cols]
+
+    # Use the earlier select box to determine if we're showing the raw
+    # dataframes
     if show_raw:
         st.subheader("Team Raw Scouting Data")
         st.write(tdf)
         st.subheader("Team Raw Pit Data")
         st.write(pdf)
 
+    # Show the pit scouting data
     st.header("Pit Scouting")
     if len(pdf.index) == 0:
+        # If we don't have data let the user know
         st.subheader('No pit scouting data')
+    # Iterate through the pit scouting results (likely only one) and show
+    # them on the UI.
     for idx, row in pdf.iterrows():
         st.subheader(row.scouter_name)
         st.info(row.robot_notes or 'no notes')
         for i in row.image_names:
             st.image(i)
 
+    # Now show our auton, teleop, and endgame dataframes in bar chart form
     st.header("Scouted Data")
     st.subheader("Auton")
     st.bar_chart(auton_df)
@@ -88,4 +103,3 @@ if team:
 
     st.subheader("Endgame")
     st.bar_chart(endgame_df)
-
