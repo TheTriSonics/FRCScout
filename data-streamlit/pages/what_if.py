f49bd456ef32e433556f49421bba42ac4a182fc6
Justin Buist <justin.buist@gmail.com>
2023-11-06 20:31:32 -0500

    Fleshing out the 'what if' section

--- data-streamlit/pages/what_if.py
+++ data-streamlit/pages/what_if.py
@@ -1,7 +1,34 @@
 import streamlit as st
 
+from scout import load_team_data, event_key
+
 
 st.set_page_config(page_title='What If')
 
-# What If Analysis
+td = load_team_data(event_key)
 
+
+def avail_teams():
+    selected_teams = []
+    for x in range(8):
+        try:
+            selected_teams += alliance[x]['team_select']
+        except KeyError:
+            pass  # Ignore
+    all_teams = [(row.number, row['name']) for idx, row in td.iterrows()]
+    rem_teams = [t for t in all_teams if t not in selected_teams]
+    return rem_teams
+
+
+alliance = {}
+for x in range(8):
+    alliance[x] = {}
+    with st.container():
+        st.subheader(f'Alliance {x+1}')
+        alliance[x]['team_select'] = st.multiselect(
+            'Members', avail_teams(),
+            max_selections=3,
+            format_func=lambda x: f'{x[0]} ({x[1]})',
+            key=f'alliance_{x}_multiselect'
+        )
+        st.info('Footer')
