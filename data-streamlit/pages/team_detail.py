fc1c96bde416bc6cc86541c8fbccd1b359ee28ea
Justin Buist <justin.buist@gmail.com>
2025-03-20 15:56:21 -0400

    Allows for no scouting data situation

--- data-streamlit/pages/team_detail.py
+++ data-streamlit/pages/team_detail.py
@@ -26,48 +26,51 @@ show_raw = st.checkbox('Show raw data')
 if team:
     scouted_data = load_event_data(get_secret_key(), get_event_key())
     (team_number, team_name) = team
-    # Trim team data down from the full event data to just theirs w/ Pandas
-    tdf = scouted_data.loc[scouted_data.scouting_team == team_number]
-    # Load in pit data
     pdf = load_pit_data(get_secret_key(), get_event_key(), team_number)
+    # Trim team data down from the full event data to just theirs w/ Pandas
+    if len(scouted_data.index) == 0:
+        st.subheader("No match data")
+    else:
+        tdf = scouted_data.loc[scouted_data.scouting_team == team_number]
+        # Load in pit data
 
-    if show_raw:
-        st.subheader("Team Raw Scouting Data")
-        st.dataframe(tdf, hide_index=True)
-        st.subheader("Team Raw Pit Data")
-        st.dataframe(pdf, hide_index=True)
+        if show_raw:
+            st.subheader("Team Raw Scouting Data")
+            st.dataframe(tdf, hide_index=True)
+            st.subheader("Team Raw Pit Data")
+            st.dataframe(pdf, hide_index=True)
 
-    # Gets a list of every column name in the dataframe
-    allcols = tdf.columns
-    # Now we can create new dataframes where we only see the auto columns
-    auton_cols = [c for c in allcols if (c.startswith("auto") and not c.endswith('total')) or c == 'match_key']
-    auton_df = tdf[auton_cols]
-    # And then teleop...
-    teleop_cols = [c for c in allcols if (c.startswith("tele") and not c.endswith('total')) or c == 'match_key']
-    teleop_df = tdf[teleop_cols]
-    # And endgame.
-    endgame_cols = [c for c in allcols if c.startswith("endgame") or c == 'match_key']
-    endgame_df = tdf[endgame_cols]
-    # And our computed ones
-    # comp_cols = [c for c in allcols if c.startswith("comp")]
-    # comp_df = tdf[comp_cols]
+        # Gets a list of every column name in the dataframe
+        allcols = tdf.columns
+        # Now we can create new dataframes where we only see the auto columns
+        auton_cols = [c for c in allcols if (c.startswith("auto") and not c.endswith('total')) or c == 'match_key']
+        auton_df = tdf[auton_cols]
+        # And then teleop...
+        teleop_cols = [c for c in allcols if (c.startswith("tele") and not c.endswith('total')) or c == 'match_key']
+        teleop_df = tdf[teleop_cols]
+        # And endgame.
+        endgame_cols = [c for c in allcols if c.startswith("endgame") or c == 'match_key']
+        endgame_df = tdf[endgame_cols]
+        # And our computed ones
+        # comp_cols = [c for c in allcols if c.startswith("comp")]
+        # comp_df = tdf[comp_cols]
 
-    # st.subheader("Computed")
-    # st.bar_chart(comp_df)
+        # st.subheader("Computed")
+        # st.bar_chart(comp_df)
 
-    st.subheader("Auton")
-    st.bar_chart(auton_df, x='match_key')
+        st.subheader("Auton")
+        st.bar_chart(auton_df, x='match_key')
 
-    st.subheader("Teleop")
-    st.bar_chart(teleop_df, x='match_key')
+        st.subheader("Teleop")
+        st.bar_chart(teleop_df, x='match_key')
 
-    st.subheader("Endgame")
-    st.bar_chart(endgame_df, x='match_key')
+        st.subheader("Endgame")
+        st.bar_chart(endgame_df, x='match_key')
 
-    st.header("Scouter Notes")
-    for idx, row in tdf.iterrows():
-        st.subheader(f'{row.scouter_name} - Match {row.match_key}')
-        st.info(row.match_notes or "No notes")
+        st.header("Scouter Notes")
+        for idx, row in tdf.iterrows():
+            st.subheader(f'{row.scouter_name} - Match {row.match_key}')
+            st.info(row.match_notes or "No notes")
 
     # Show the pit scouting data
     st.header("Pit Scouting")
