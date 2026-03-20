7e37ec91e251494c7c70930efbe9fee711a1844a
Justin Buist <justin.buist@gmail.com>
2026-03-20 10:50:04 -0400

    Remove drop dupes -- causes error with our new list data

--- trisonics-api/function_app.py
+++ trisonics-api/function_app.py
@@ -518,7 +518,7 @@ def get_scouting_data(secret_team_key=None, event_key=None):
         for c in bool_to_int_cols:
             if c in df.columns:
                 df[c] = df[c].astype(int)
-        df.drop_duplicates(inplace=True, ignore_index=True, keep='last')
+        # df.drop_duplicates(inplace=True, ignore_index=True, keep='last')
         df.fillna(inplace=True, value=0)
     return df
 
